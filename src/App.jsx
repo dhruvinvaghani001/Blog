b63e88f4297808bd1480eea73cba4bd6d31e4758
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2023-10-07 15:24:49 +0530

    updater

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-
+    console.log("hello");
     categorySerive.getCategories().then((category) => {
       console.log(category);
       if (category) {
