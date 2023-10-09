09765484f60194bef7b1208e9ff64c13d46e9cbe
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2023-10-09 17:07:19 +0530

    header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,19 +43,19 @@ const Header = () => {
 
     return (
         <>
-            <nav className={`bg-newhite  border-b-2 border-indigo-500`}>
+            <nav className="bg-newhite  border-b-2 border-indigo-500">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to="/" className="flex items-center">
                         <img src="/vite.svg" className="h-8 mr-3" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap ">Blogger</span>
                     </Link>
-                    <button data-collapse-toggle="navbar-default" type="button" onClick={() => { setacClass((prev)=>!prev) }} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-default" type="button" onClick={() => { setacClass((prev) => !prev) }} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div className={`${acclass ? "active" : "inactive" } hidden w-full md:block md:w-auto" id="navbar-default`}>
+                    <div className={`${!acclass ? "hidden" : null} w-full md:block md:w-auto`}>
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
 
                             {
@@ -83,3 +83,6 @@ const Header = () => {
 }
 
 export default Header
+
+
+// {`${acclass ? "active" : "inactive" } hidden w-full md:block md:w-auto" id="navbar-default`}
